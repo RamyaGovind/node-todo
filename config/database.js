5ee2144bc729f8821c3e99b945e90af06921021c
Ramya <ramya@voonik.com>
2015-08-20 15:07:07 +0530

    db url fix

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 
 	// the database url to connect
-	//url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'
-	url : 'mongodb://Ramya:voonik123@ec2-54-179-236-120.ap-southeast-1.compute.amazonaws.com:27017/dummyDB'
+	//url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/dummy'
+	url : 'mongodb://Ramya1:voonik123@ec2-54-179-236-120.ap-southeast-1.compute.amazonaws.com:27017/dummyDB'
 }
