56a50b0b8a4d53ea141f2404fc41ac3ece481e70
RamyaGovind <ramya@voonik.com>
2015-09-02 15:29:50 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,5 +3,5 @@ module.exports = {
 	// the database url to connect
 	//url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/dummy'
 	//url : 'mongodb://Ramya1:voonik123@ec2-54-179-236-120.ap-southeast-1.compute.amazonaws.com:27017/dummyDB'
-	url : 'mongodb://my-app-db/dbname'
+	url : 'mongodb://ubuntu@my-app-db/dbname'
 }
